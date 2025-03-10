850f1fbeefa2510e80d98b5464cd2a4bb6a0ac19
loud3stsil3nce <rafiurrahman325@gmail.com>
2025-03-10 02:37:11 -0400

    Added tasks table.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import Optional
 import sqlalchemy as sa 
 import sqlalchemy.orm as so
@@ -9,5 +10,23 @@ class User(db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     
+    tasks: so.WriteOnlyMapped['Task'] = so.relationship(
+        back_populates='author'
+    )
+    
+    
+    
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    
+    
+class Task(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(500))
+    timestamp: so.Mapped[datetime] = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
+    
+    author: so.Mapped[User] = so.relationship(back_populates='tasks')
+    
+    def __repr__(self):
+        return '<Task {}>'.format(self.body)
